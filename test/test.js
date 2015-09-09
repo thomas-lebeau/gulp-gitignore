af38adf52cbaea91f8a858d826f238f2c4929189
Thomas Lebeau <lebeau.thomas@gmail.com>
2015-09-09 17:29:44 +0300

    Add test for a dependency

--- test/test.js
+++ test/test.js
@@ -35,7 +35,7 @@ describe('gitignore()', function () {
     stream.end();
   });
 
-  it.skip('should work with custom gitignore path', function (cb) {
+  it('should accept custom gitignore file', function (cb) {
     var stream = gitignore('test/.gitignore-custom');
     var buffer = [];
 
@@ -56,12 +56,12 @@ describe('gitignore()', function () {
     stream.write(fake('foo/b.txt'));
     stream.write(fake('bar/a.txt'));
     stream.write(fake('foo.log'));
-    stream.write(fake('baz/foo.log'));
+    // stream.write(fake('baz/foo.log'));
 
     stream.end();
   });
 
-  it.skip('should work with dotfiles', function (cb) {
+  it('should work with dotfiles', function (cb) {
     var stream = gitignore('test/.gitignore-custom');
     var buffer = [];
 
@@ -84,15 +84,52 @@ describe('gitignore()', function () {
     stream.write(fake('bar/a.txt'));
     stream.write(fake('.gitkeep'));
     stream.write(fake('.DS_Store'));
-    stream.write(fake('baz/.DS_Store'));
+    // stream.write(fake('baz/.DS_Store'));
 
     stream.end();
   });
 
-  it('should trow error', function (cb) {
+  it('should trow proper error if gitignore path is invalid', function (cb) {
     (function () {
       gitignore('non-existant/.gitignore');
     }).should.throw()
     cb();
   });
+});
+
+/**
+ * It will fail untill gitignore-parser() fixes its bug.
+ * @see https://github.com/codemix/gitignore-parser/pull/10
+ */
+describe('[dependencies] gitignore-parser()', function () {
+  it('should ignore files in all subfolders', function (cb) {
+    var stream = gitignore('test/.gitignore-custom');
+    var buffer = [];
+
+    stream.on('data', function (file) {
+      buffer.push(file);
+    });
+
+    stream.on('end', function () {
+      buffer.should.have.length(3);
+      buffer.should.containEql(fake('foo.txt'));
+      buffer.should.containEql(fake('foo/a.txt'));
+      buffer.should.containEql(fake('.gitkeep'));
+      cb();
+    });
+
+    stream.write(fake('foo.txt'));
+    stream.write(fake('bar.txt'));
+    stream.write(fake('foo/a.txt'));
+    stream.write(fake('foo/b.txt'));
+    stream.write(fake('bar/a.txt'));
+    stream.write(fake('.gitkeep'));
+    stream.write(fake('.DS_Store'));
+
+    // these files should be ignored
+    stream.write(fake('baz/foo.log'));
+    stream.write(fake('baz/.DS_Store'));
+
+    stream.end();
+  })
 });
